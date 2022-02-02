45b0035f0a97598e3b141378bafe4ea3f78e6e00
Tijl Van den Brugghen <me@tijlvdb.me>
2022-02-02 21:44:46 +0100

    feat: put add links form into an actual form

--- frontend/src/pages/PageLinks.tsx
+++ frontend/src/pages/PageLinks.tsx
@@ -8,7 +8,62 @@ import { Api } from '../state/api'
 import { Link } from '../state/api-types'
 import { queryClient } from '../state/query-client'
 
-const columns = [
+export function PageLinks() {
+	return (
+		<>
+			<AddLinksForm />
+			<LinksTable />
+		</>
+	)
+}
+
+function AddLinksForm() {
+	const [longUrl, setLongUrl] = useState('')
+	const [shortUrl, setShortUrl] = useState('')
+
+	const create = useMutation('linkCreate', async () => {
+		await Api.linksCreate({ link: shortUrl, target: longUrl })
+		queryClient.invalidateQueries('links')
+		setLongUrl('')
+		setShortUrl('')
+	})
+
+	return (
+		<>
+			{create.isError && (
+				<p className="mb-4 bg-red-600 text-white rounded p-2 px-3 shadow font-semibold">
+					{'' + create.error}
+				</p>
+			)}
+
+			<form
+				onSubmit={e => {
+					e.preventDefault()
+					create.mutate()
+				}}
+			>
+				<div className="md:grid grid-cols-6 gap-4 mb-4">
+					<div className="col-span-3">
+						<Input label="Long URL" value={[longUrl, setLongUrl]} />
+					</div>
+					<div className="col-span-2 mt-4 md:mt-0">
+						<Input label="Short URL" value={[shortUrl, setShortUrl]} />
+					</div>
+					<div className="flex items-end mt-4 md:mt-0">
+						<Button
+							className="w-full"
+							disabled={longUrl === '' || shortUrl === '' || create.isLoading}
+						>
+							Add
+						</Button>
+					</div>
+				</div>
+			</form>
+		</>
+	)
+}
+
+const linksTableColumns = [
 	{
 		name: 'Short URL',
 		cell: (row: Link) => (
@@ -41,56 +96,8 @@ const columns = [
 	}
 ]
 
-export function PageLinks() {
-	return (
-		<>
-			<AddLinksForm />
-			<LinksTable />
-		</>
-	)
-}
-
-function AddLinksForm() {
-	const [longUrl, setLongUrl] = useState('')
-	const [shortUrl, setShortUrl] = useState('')
-
-	const create = useMutation('linkCreate', async () => {
-		await Api.linksCreate({ link: shortUrl, target: longUrl })
-		queryClient.invalidateQueries('links')
-		setLongUrl('')
-		setShortUrl('')
-	})
-
-	return (
-		<>
-			{create.isError && (
-				<p className="mb-4 bg-red-600 text-white rounded p-2 px-3 shadow font-semibold">
-					{'' + create.error}
-				</p>
-			)}
-
-			<div className="md:grid grid-cols-6 gap-4 mb-4">
-				<div className="col-span-3">
-					<Input label="Long URL" value={[longUrl, setLongUrl]} />
-				</div>
-				<div className="col-span-2 mt-4 md:mt-0">
-					<Input label="Short URL" value={[shortUrl, setShortUrl]} />
-				</div>
-				<div className="flex items-end mt-4 md:mt-0">
-					<Button
-						className="w-full"
-						disabled={longUrl === '' || shortUrl === '' || create.isLoading}
-						onClick={() => create.mutate()}
-					>
-						Add
-					</Button>
-				</div>
-			</div>
-		</>
-	)
-}
-
 function LinksTable() {
+	// page is unused for now
 	const [page, setPage] = useState(0)
 	const [selectedRows, setSelectedRows] = useState([] as Link[])
 	const [toggleCleared, setToggleCleared] = useState(false)
@@ -126,7 +133,7 @@ function LinksTable() {
 				</div>
 			</div>
 			<DataTable
-				columns={columns}
+				columns={linksTableColumns}
 				data={links.data?.links || []}
 				dense
 				selectableRows
